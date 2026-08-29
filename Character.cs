9553a6a727cf2f2b5c958e6c380a9bca979fd60f
Max Jöhnk <maxjoehnk@gmail.com>
2026-08-29 15:40:51 +0200

    wip: add sound track and effects

--- Character.cs
+++ Character.cs
@@ -1,4 +1,5 @@
 using Godot;
+using Godot.Collections;
 
 namespace BrackeysGameJam2026;
 
@@ -7,7 +8,11 @@ public partial class Character : Button
 {
 	[Export(PropertyHint.MultilineText)] public string Hint { get; set; }
 	[Export] public bool Murderer { get; set; }
-	
+
+	[Export] public Array<AudioStream> Samples { get; set; } = [];
+
+	private AudioStreamPlayer AudioPlayer => this.GetNode<AudioStreamPlayer>("AudioStreamPlayer");
+
 	private TextureRect ButtonTexture => this.GetNode<TextureRect>("TextureRect");
 
 	[Export]
@@ -15,5 +20,24 @@ public partial class Character : Button
 	{
 		get => this.ButtonTexture.Texture;
 		set => this.ButtonTexture.Texture = value;
+	}
+
+	public override void _Ready()
+	{
+		AudioStreamRandomizer stream = new()
+		{
+			StreamsCount = this.Samples.Count,
+		};
+		for (int i = 0; i < this.Samples.Count; i++)
+		{
+			stream.AddStream(i, this.Samples[i]);
+		}
+
+		this.AudioPlayer.Stream = stream;
+
+		if (this.Samples.Count > 0)
+		{
+			this.Pressed += () => this.AudioPlayer.Play();
+		}
 	}
 }
