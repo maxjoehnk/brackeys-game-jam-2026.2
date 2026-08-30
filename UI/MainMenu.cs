cdb81d4701c0a2bb4d4f40ce02d33fb0c640964a
Max Jöhnk <maxjoehnk@gmail.com>
2026-08-30 09:43:06 +0200

    hide exit button in web builds

--- UI/MainMenu.cs
+++ UI/MainMenu.cs
@@ -6,10 +6,16 @@ namespace BrackeysGameJam2026.UI;
 public partial class MainMenu : Control
 {
 	private Control? PlayButton => this.FindChild("Play") as Control;
+	private Control? ExitButton => this.FindChild("Exit") as Control;
 
 	public override void _Ready()
 	{
 		this.PlayButton?.GrabFocus();
+
+		if (OS.GetName() == "Web")
+		{
+			this.ExitButton?.Hide();
+		}
 	}
 
 	public void OnPlay()
